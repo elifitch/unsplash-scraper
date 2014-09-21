410da646903653a45000004d70431f8cbc41e3a2
eli <eli@EliMBPR.local>
2014-09-21 11:52:27 -0400

    accounting for new classes and auth procedure on unsplash

--- unsplash-scraper.js
+++ unsplash-scraper.js
@@ -4,21 +4,28 @@ var url = require('url');
 var exec = require('child_process').exec;
 
 (function() {
-    var dlDir = './downloads';
-    var host = 'http://unsplash.com/';
+    var dlDir = './downloads/';
+    var host = 'http://unsplash.com';
     //grabs all images and stores in array
     request(host, function(err, resp, body) {
         if (err)
             throw err;
         $ = cheerio.load(body);
-        $('.photo_div img').each(function(){
-            var fileUrl = $(this).attr('src');
-            var fileName = url.parse(fileUrl).pathname.split('/').pop();
-            var wget = 'wget -P ' + dlDir + ' ' + fileUrl;
-            var child = exec(wget, function(err, stdout, stderr) {
-                if (err) throw err;
-                else console.log(fileName + ' downloaded to ' + dlDir);
-            });
+        $('.photo a').each(function(){
+            var dlPage = $(this).attr('href');
+
+            request(host+dlPage,function(err,resp,body){
+                //redirects image
+                var fileUrl = resp.request.uri.href;
+                var fileName = url.parse(fileUrl).pathname.split('/').pop().split('?').shift();
+                //in wget we have to escape '&'
+                var wget = 'wget -P ' + dlDir + ' ' + fileUrl.replace(/&/g,'\\&') + ' -O ' + dlDir+fileName;
+                var child = exec(wget, function(err, stdout, stderr) {
+                    if (err){ console.log(stderr); throw err; } 
+                    else console.log(fileName + ' downloaded to ' + dlDir);
+                    // else console.log(stdout+'; \n '+stderr);
+                });
+            })
         });        
     });
 }());
