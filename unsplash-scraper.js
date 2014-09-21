017f55b41ef07a8191e5610c5f6e6a29fa0494e0
eli <eli@EliMBPR.local>
2014-09-21 11:58:51 -0400

    moving from wget to curl for broader support

--- unsplash-scraper.js
+++ unsplash-scraper.js
@@ -6,10 +6,10 @@ var exec = require('child_process').exec;
 (function() {
     var dlDir = './downloads/';
     var host = 'http://unsplash.com';
-    //grabs all images and stores in array
+    
     request(host, function(err, resp, body) {
-        if (err)
-            throw err;
+        if (err) throw err;
+
         $ = cheerio.load(body);
         $('.photo a').each(function(){
             var dlPage = $(this).attr('href');
@@ -18,12 +18,11 @@ var exec = require('child_process').exec;
                 //redirects image
                 var fileUrl = resp.request.uri.href;
                 var fileName = url.parse(fileUrl).pathname.split('/').pop().split('?').shift();
-                //in wget we have to escape '&'
-                var wget = 'wget -P ' + dlDir + ' ' + fileUrl.replace(/&/g,'\\&') + ' -O ' + dlDir+fileName;
-                var child = exec(wget, function(err, stdout, stderr) {
+                //in curl we have to escape '&' from fileUrl
+                var curl =  'curl ' + fileUrl.replace(/&/g,'\\&') +' -o ' + dlDir+fileName + ' --create-dirs';
+                var child = exec(curl, function(err, stdout, stderr) {
                     if (err){ console.log(stderr); throw err; } 
                     else console.log(fileName + ' downloaded to ' + dlDir);
-                    // else console.log(stdout+'; \n '+stderr);
                 });
             })
         });        
